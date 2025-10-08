5ce3e6dc2f13e49917cdc14c552b6a9e3e620ea1
Егор Янковский <yankovskij.2019@azure.nstu.ru>
2025-10-08 16:42:18 +0700

    Added comments

--- SciDevOOP/Optimizators/MinimizerMonteCarlo.cs
+++ SciDevOOP/Optimizators/MinimizerMonteCarlo.cs
@@ -8,20 +8,30 @@ namespace SciDevOOP.Optimizators;
 
 class MinimizerMonteCarlo : IOptimizator
 {
-    public int MaxIter = 100000;
+    public int MaxIter = 100_000;
+    
     public IVector Minimize(IFunctional objective, IParametricFunction function, IVector initialParameters, IVector minimumParameters = null, IVector maximumParameters = null)
     {
+        // 1. Инициализация начальных данных для итерационного цикла.
         var param = new Vector();
         var minparam = new Vector();
+        
         foreach (var p in initialParameters) param.Add(p);
         foreach (var p in initialParameters) minparam.Add(p);
+        
         var fun = function.Bind(param);
         var currentmin = objective.Value(fun);
+        
         var rand = new Random(0);
+
+        // 2. Сам цикл решения.
         for (int i = 0; i < MaxIter; i++)
         {
+            // Генерируем рандомные коэффициенты.
             for (int j = 0; j < param.Count; j++) param[j] = rand.NextDouble();
+            // Находим значение функционала
             var f = objective.Value(function.Bind(param));
+            // Обновляем параметры функции если значение функционала меньше.
             if (f < currentmin)
             {
                 currentmin = f;
--- SciDevOOP/Program.cs
+++ SciDevOOP/Program.cs
@@ -3,11 +3,17 @@ using SciDevOOP.Optimizators;
 using SciDevOOP.Functions;
 using SciDevOOP.MathematicalObjects;
 
+
+// 1. Выбор метода оптимизации.
 var optimizer = new MinimizerMonteCarlo();
+
+// 2. Начальные параметры для целевой функции.
 var initial = new Vector { 0.5,
                            0.1, 0.2,
                            0.1, 0.1,
                            0.006, 0.004};
+
+// 3. Вводим точки, по которым строим сплайн (и прочее...).
 var n = int.Parse(Console.ReadLine());
 List<(double x, double y)> points = [];
 for (var i = 0; i < n; i++)
@@ -15,10 +21,16 @@ for (var i = 0; i < n; i++)
     var str = Console.ReadLine()?.Split();
     points.Add((double.Parse(str[0]), double.Parse(str[1])));
 }
+
+// 4. Выбираем функционал. В качестве параметров обязательно нужны точки из п.3.
 var functional = new MyFunctional() { points = points };
+
+// 5. Выбор целевой функции.
 var fun = new PiecewiseLinearFunction();
 
+// 6. Решение задачи оптимизации.
 var res = optimizer.Minimize(functional, fun, initial);
 
+// 7. Вывод результата. (Целевые коэффициенты для функции).
 foreach (var r in res)
     Console.WriteLine(r);
