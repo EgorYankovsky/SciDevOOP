8194cfa101d6c788f06f9a41cbaa41120e20dece
Artyom <art-sav2002@yandex.ru>
2025-10-23 17:40:06 +0700

    IntegrationNorm

--- SciDevOOP/Functionals/IntegrationNorm.cs
+++ SciDevOOP/Functionals/IntegrationNorm.cs
@@ -6,67 +6,149 @@ namespace SciDevOOP.Functionals;
 
 class IntegrationNorm : IFunctional
 {
-    public List<(double x, double y)> points; // Точки для численного интегрирования
-    public double LowerBound { get; set; }
-    public double UpperBound { get; set; }
-    public int NumberOfPoints { get; set; } = 1000;
+    private IList<IList<double>> points; // Точки для численного интегрирования
+    public IList<double> LowerBound { get; set; }
+    public IList<double> UpperBound { get; set; }
+    public IList<int> NumberOfPoints { get; set; }
 
-    public IntegrationNorm(double lowerBound, double upperBound, int numberOfPoints = 1000)
+    public IntegrationNorm(IList<double> lowerBound, IList<double> upperBound, IList<int> numberOfPoints = null)
     {
         LowerBound = lowerBound;
         UpperBound = upperBound;
-        NumberOfPoints = numberOfPoints;
+        if (numberOfPoints == null)
+        {
+            NumberOfPoints = new List<int>();
+            for (int i = 0; i<lowerBound.Count; i++)
+            {
+                NumberOfPoints.Add(1000);
+            }
+        }
+        else
+        {
+            NumberOfPoints = numberOfPoints;
+        }
     }
 
     double IFunctional.Value(IFunction function)
     {
         // L² норма ошибки: sqrt(∫|f(x) - y(x)|² dx)
         // где y(x) - интерполяция данных точек
-        double integral = SimpsonIntegration(function, LowerBound, UpperBound, NumberOfPoints);
+        double integral = MultiDimensionalIntegration(function, LowerBound, UpperBound, NumberOfPoints);
         return Math.Sqrt(integral);
     }
 
-    private double SimpsonIntegration(IFunction function, double a, double b, int n)
+    private double MultiDimensionalIntegration(IFunction function, IList<double> lower, IList<double> upper, IList<int> nPoints)
     {
+        int dimesions = lower.Count;
+
+        // Рекурсивное интегрирование по всем измерениям
+        return RecursiveIntegration(function, lower, upper, nPoints, 0, new List<double>());
+    }
+
+    private double RecursiveIntegration(IFunction function, IList<double> lower, IList<double> upper, IList<int> nPoints, int currentDim, List<double> currentPoint)
+    {
+        if (currentDim == lower.Count)
+        {
+            // Для полной размерности вычисляем подынтегральное выражение
+            return SquareError(function, currentPoint);
+        }
+
+        double a = lower[currentDim];
+        double b = upper[currentDim];
+        int n = nPoints[currentDim];
+
         if (n % 2 == 1) n++;
 
         double h = (b - a) / n;
-        double sum = SquareError(function, a) + SquareError(function, b);
+        double sum = 0;
 
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i <= n; i++)
         {
             double x = a + i * h;
-            double value = SquareError(function, x);
+            List<double> newPoint = new List<double>(currentPoint) { x };
 
-            if (i % 2 == 0)
-                sum += 2 * value;
-            else
-                sum += 4 * value;
+            double weight = GetSimpsonWeight(i, n);
+            double partialIntegral = RecursiveIntegration(function, lower, upper, nPoints, currentDim + 1, newPoint);
+
+            sum += weight * partialIntegral;
         }
 
         return sum * h / 3;
     }
 
-    private double SquareError(IFunction function, double x)
+    private double GetSimpsonWeight(int i, int n)
     {
+        if (i == 0 || i == n) return 1;
+        return (i % 2 == 0) ? 2 : 4;
+    }
+
+
+    private double SquareError(IFunction function, IList<double> point)
+    {
+        Vector input = new Vector();
+        foreach (double coord in point)
+        {
+            input.Add(coord);
+        }
+        double functionValue = function.Value(input);
         // Находим ближайшую точку для y(x)
-        double targetY = InterpolateY(x);
-        double functionValue = function.Value(new Vector() { x });
-        double error = functionValue - targetY;
+        double targetValue = InterpolateValue(point);
+        double error = functionValue - targetValue;
         return error * error;
     }
 
-    private double InterpolateY(double x)
+    private double InterpolateValue(IList<double> point)
     {
+        if (!point.Any()) return 0; /////////////////////////
+
+        double minDistance = double.MaxValue;
+        double bestValue = 0;
         // Простая линейная интерполяция
-        for (int i = 0; i < points.Count - 1; i++)
+        foreach (var dataPoint in points)
         {
-            if (x >= points[i].x && x <= points[i + 1].x)
+            double distance = CalculateDistance(point, dataPoint);
+            if (distance < minDistance)
             {
-                double t = (x - points[i].x) / (points[i + 1].x - points[i].x);
-                return points[i].y + t * (points[i + 1].y - points[i].y);
+                minDistance = distance;
+                // Предполагаем, что последняя координата - значение функции
+                bestValue = dataPoint[dataPoint.Count - 1];
             }
+            //    if (x >= points[i].x && x <= points[i + 1].x)
+            //    {
+            //        double t = (x - points[i].x) / (points[i + 1].x - points[i].x);
+            //        return points[i].y + t * (points[i + 1].y - points[i].y);
+            //    }
+            //}
+            //return points[0].y; // fallback
         }
-        return points[0].y; // fallback
+        return bestValue;
+    }
+
+    private double CalculateDistance(IList<double> point1, IList<double> point2)
+    {
+        double sum = 0;
+        int minDim = Math.Min(point1.Count, point2.Count);
+
+        for (int i = 0; i < minDim; i++)
+        {
+            double diff = point1[i] - point2[i];
+            sum += diff * diff;
+        }
+
+        return Math.Sqrt(sum);
+    }
+
+    // Метод для установки точек данных
+    public void SetDataPoints(IList<IList<double>> dataPoints)
+    {
+        this.points = dataPoints;
+    }
+
+    // Альтернативный конструктор с точками данных
+    public IntegrationNorm(IList<double> lowerBounds, IList<double> upperBounds,
+                          IList<IList<double>> dataPoints, IList<int> numberOfPoints = null)
+        : this(lowerBounds, upperBounds, numberOfPoints)
+    {
+        SetDataPoints(dataPoints);
     }
 }
