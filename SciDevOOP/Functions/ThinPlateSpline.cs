a93beb0e2a792fb9f61c6db941c19734d7bb179b
Егор <egeryankov@gmail.com>
2025-11-18 20:20:23 +0700

    TPS tested on 1D and 2D examples

--- SciDevOOP/Functions/ThinPlateSpline.cs
+++ SciDevOOP/Functions/ThinPlateSpline.cs
@@ -29,7 +29,7 @@ class ThinPlateSpline : IParametricFunction
         {
             if (point1.Count != ponit2.Count) throw new ArgumentException($"Point1 dimension {point1.Count} isn't equal to point2 dimension {ponit2.Count}.");
             var sum = 0.0D;
-            for (int i = 0; i < point1.Count - 1; ++i)
+            for (int i = 0; i < point1.Count; ++i)
                 sum  += Math.Pow(point1[i] - ponit2[i], 2);
             return Math.Sqrt(sum);
 
@@ -40,41 +40,13 @@ class ThinPlateSpline : IParametricFunction
             var sb = new StringBuilder();
             var A = new Matrix();
             var b = new Vector();
-            var solver = new Gauss();   // ???
+            var solver = new Gauss();
 
             var dimension = points.Count / PointsAmount;
 
-            for (int i = 0; i < PointsAmount + dimension; ++i)
-            {
-                A.Add([]);
-                var pnt1 = new Vector(points.Skip(i * dimension).Take(dimension));
-                for (int j = 0; j < PointsAmount + dimension; ++j)
-                {
-                    if (i > j) A[i].Add(A[j][i]);
-                    else if (i == j) A[i].Add(0.0D);
-                    else 
-                    {
-                        if (i < PointsAmount && j < PointsAmount)
-                        {
-                            var pnt2 = new Vector(points.Skip(j * dimension).Take(dimension));
-                            A[i].Add(Phi(Distance(pnt1, pnt2)));
-                        }
-                        else if (i < PointsAmount && !(j < PointsAmount))
-                        {
-                            if(j == PointsAmount) A[i].Add(1.0D);
-                            else A[i].Add(pnt1[j - PointsAmount - 1]);
-                        }
-                        else A[i].Add(0.0D);
-                    }
-                }
-            }
-
-            for (int i = 0; i < PointsAmount + dimension; ++i)
-            {
-                if (i < PointsAmount) b.Add(new Vector(points.Skip(i * dimension).Take(dimension))[^1]);
-                else b.Add(0.0D);
-            }
-
+            Task matrixGeneration = Task.Run(() => GenerateMatrix(A, PointsAmount, dimension, points));
+            Task vectorGeneration = Task.Run(() => GenerateVector(b, PointsAmount, dimension, points));
+            Task.WaitAll(matrixGeneration, vectorGeneration);
 
             var x = solver.Solve(A, b).Select(xi => -1.0 * xi);
 
@@ -87,19 +59,59 @@ class ThinPlateSpline : IParametricFunction
             };
         }
 
+        private static void GenerateMatrix(Matrix A, int PointsAmount, int dimension, IVector points)
+        {
+            
+            for (int i = 0; i < PointsAmount + dimension; ++i)
+            {
+                A.Add([]);
+                var pnt1 = new Vector(points.Skip(i * dimension).Take(dimension).SkipLast(1));
+                for (int j = 0; j < PointsAmount + dimension; ++j)
+                {
+                    if (i > j) A[i].Add(A[j][i]);
+                    else if (i == j) A[i].Add(0.0D);
+                    else 
+                    {
+                        if (i < PointsAmount && j < PointsAmount)
+                        {
+                            var pnt2 = new Vector(points.Skip(j * dimension).Take(dimension).SkipLast(1));
+                            A[i].Add(Phi(Distance(pnt1, pnt2)));
+                        }
+                        else if (i < PointsAmount && !(j < PointsAmount))
+                        {
+                            if(j == PointsAmount) A[i].Add(1.0D);
+                            else A[i].Add(pnt1[j - PointsAmount - 1]);
+                        }
+                        else A[i].Add(0.0D);
+                    }
+                }
+            }
+        }
+
+        public static void GenerateVector(IVector b, int PointsAmount, int dimension, IVector points)
+        {
+            for (int i = 0; i < PointsAmount + dimension; ++i)
+            {
+                if (i < PointsAmount) b.Add(new Vector(points.Skip(i * dimension).Take(dimension))[^1]);
+                else b.Add(0.0D);
+            }
+        }
+
         private static double Phi(double t) => t > 0 ? t * t * Math.Log(t) : t == 0 ? 0 : throw new Exception($"Input value must be positive, but has {t}");
 
         public double Value(IVector point)
         {
             if (Points is null) throw new NullReferenceException("Vector of points wasn't initialized."); 
-            if (Points.Count / PointsAmount != point.Count) throw new ArgumentException("Point dimension isn't equal to spline dimension.");
+            if (Points.Count / PointsAmount != point.Count + 1) throw new ArgumentException("Point dimension isn't equal to spline dimension.");
             if (w is null) throw new NullReferenceException("Vector of weights wasn't initialized.");
             var ans = a?[0] ?? throw new NullReferenceException("Vector of coefcicients wasn't initialized.");
+
+            var dimension = point.Count + 1;
 
             for (int i = 0; i < point.Count; ++i) ans += point[i] * a[i + 1];
 
             for (int j = 0; j < PointsAmount; ++j) 
-                ans += w[j] + Phi(Distance(point, new Vector(Points.Skip(j * PointsAmount).Take(point.Count))));
+                ans += w[j] * Phi(Distance(point, new Vector(Points.Skip(j * dimension).Take(point.Count))));
 
             return ans;
         }
